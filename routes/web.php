5f3643b84d331794f1380d2df3152f9841de2aba
Cgarortg <cgar.ortg@gmail.com>
2025-05-06 15:49:52 +0200

    Rutas users

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Auth::routes();
+
+Route::get(config('global.SINUSER'), [App\Http\Controllers\HomeController::class, 'inicio'])->name('inicio');
+Route::get(config('global.NORMAL'), [App\Http\Controllers\HomeController::class, 'inicioUser'])->name('inicioUser');
+Route::get(config('global.ADMINISTRADOR'), [App\Http\Controllers\HomeController::class, 'inicioAdmin'])->name('inicioAdmin');
+Route::get(config('global.ROOT'), [App\Http\Controllers\HomeController::class, 'inicioRoot'])->name('inicioRoot');
+
+
